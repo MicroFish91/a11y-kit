c0b37edb7f69703fe82e19276d5137775f28ad0a
Matthew Fisher <40250218+MicroFish91@users.noreply.github.com>
2022-09-02 15:30:48 -0700

    wireup sandbox menu (#5)

--- src/extensionVariables.ts
+++ src/extensionVariables.ts
@@ -2,6 +2,5 @@ import { Menu } from "./menu/Menu";
 
 export namespace ext {
     export let a11yMenu: Menu;
-    // Populate later...
     export let sandboxMenu: Menu;
 }
--- src/menu/createAndRegisterMenu.ts
+++ src/menu/createAndRegisterMenu.ts
@@ -6,6 +6,12 @@ import { Menu } from "./Menu";
 
 export function createAndRegisterA11yMenu(): Menu {
   const a11yMenu = new Menu();
-  a11yMenu.addComponents(qpComponent, ibComponent, tvComponent, wpComponent);
+  a11yMenu.addComponents(qpComponent, ibComponent, tvComponent);
   return a11yMenu;
+}
+
+export function createAndRegisterSandboxMenu(): Menu {
+  const sandboxMenu = new Menu();
+  sandboxMenu.addComponents(qpComponent, ibComponent, tvComponent, wpComponent);
+  return sandboxMenu;
 }
