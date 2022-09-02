1b6a432081b583825a594ff281dba861dbad9b55
MicroFish91 <mwfisher91@gmail.com>
2022-09-02 15:30:03 -0700

    wireup sandbox menu

--- src/commands/registerCommands.ts
+++ src/commands/registerCommands.ts
@@ -5,6 +5,7 @@ import { treeCommands } from "./treeCommands";
 export function registerCommands(context: ExtensionContext) {
   context.subscriptions.concat([
     commands.registerCommand("a11y-kit.menu", async (context: ExtensionContext) => await ext.a11yMenu.display(context)),
+    commands.registerCommand("a11y-kit.sandbox", async (context: ExtensionContext) => await ext.sandboxMenu.display(context)),
     ...treeCommands(),
   ]);
 }
--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,12 @@
 import * as vscode from "vscode";
 import { registerCommands } from "./commands/registerCommands";
 import { ext } from "./extensionVariables";
-import { createAndRegisterA11yMenu } from "./menu/createAndRegisterMenu";
+import { createAndRegisterA11yMenu, createAndRegisterSandboxMenu } from "./menu/createAndRegisterMenu";
 
 export function activate(context: vscode.ExtensionContext) {
   console.log("Starting a11y-kit extension.");
   ext.a11yMenu = createAndRegisterA11yMenu();
+  ext.sandboxMenu = createAndRegisterSandboxMenu();
 
   registerCommands(context);
 }
